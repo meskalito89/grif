ed983c5cbf3dca4ebed0a79f4885b6b48a57f073
meskalito89 <rokinea89mone@mail.ru>
2022-05-30 15:44:56 +0500

    trying add more type hinting in code

--- new_grif.py
+++ new_grif.py
@@ -1,24 +1,37 @@
+from typing import Literal
 from colorama import Fore
 
-note_sequence = ('c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b')
+note_sequence = Literal['c', 'c#', 'd', 'd#', 'e', 'f', 'f#',
+                        'g', 'g#', 'a', 'a#', 'b']
+
+
+
+def colorize_note(char, color=Fore.WHITE):
+    """this function get_schar and wrap it in Fore.color"""
+    return Fore.__dict__[color.upper()] + char + Fore.WHITE
+
 
 class Grif:
-    def __init__(self, tune=('e','b','g','d','a','e'), lad_width=7, n_of_lads=15) -> None:
+
+    """This is class that show notes on griff of the guitar"""
+    def __init__(self,
+                 tune=('e',  'b', 'g', 'd', 'a', 'e'),
+                 lad_width=3,
+                 n_of_lads=15
+                 ) -> None:
         self.lad_width = lad_width
         self.n_of_lads = n_of_lads
         self.tune = tune
-        self._colors = dict()
+        self._colors: dict[str, str] = {}
         self._text = ''
-        
-    def colorize_note(self, note, color=Fore.WHITE):
-        return Fore.__dict__[color.upper()] + note + Fore.WHITE
+
 
     def get_strings(self):
         strings = []
         for open_string_note in self.tune:
             string = []
             index = note_sequence.index(open_string_note)
-            for i  in range(1, self.n_of_lads + 1):
+            for i in range(1, self.n_of_lads + 1):
                 note = note_sequence[(index + i) % len(note_sequence)]
                 string.append(note)
             strings.append(string)
@@ -27,20 +40,23 @@ class Grif:
     def update_text(self):
         self._text = ''
         strings = self.get_strings()
-        lad_width_extension_for_color=10
+        lad_width_extension_for_color = 10
         for string in strings:
             for note in string:
                 if note in self._colors:
                     color = self._colors.get(note, 'WHITE')
-                    note = self.colorize_note(note, color)
+                    note = colorize_note(note, color)
                 else:
-                    note = self.colorize_note('-', "white")
-                self._text += note.center(self.lad_width + lad_width_extension_for_color, '-') + '|'
+                    note = colorize_note('-', "white")
+                self._text += note.center(
+                    self.lad_width +
+                    lad_width_extension_for_color, '-') + '|'
             self._text += '\n'
         self.add_lad_numbers()
 
     def add_lad_numbers(self):
-        lad_numbers = ('','','III','','V','','VII','','IX','','','XII','','','XV')
+        lad_numbers = ('', '', 'III', '', 'V', '', 'VII', '',
+                       'IX', '', '', 'XII', '', '', 'XV')
         lad_numbers_string = ''
         for lad_number in lad_numbers:
             lad_numbers_string += lad_number.center(self.lad_width + 1, ' ')
@@ -51,5 +67,13 @@ class Grif:
             self._colors[note] = color
         self.update_text()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return '\n' + self._text + '\n'
+
+
+if __name__ == "__main__":
+    grif = Grif(tune=('g', 'd', 'a', 'e'))
+    grif.add_chord(('c', 'e', 'g', 'a#'), 'green')
+    grif.add_chord(('g', 'b', 'd', 'f'), 'yellow')
+    print(grif)
+
